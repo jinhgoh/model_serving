6bb6bb7a2ee0c4c759350c3fd14a82c798e536de
Jinhyong Goh <68317603+jinhgoh@users.noreply.github.com>
2022-09-03 14:46:09 +0900

    Update pytorch_flask.py

--- pytorch_flask.py
+++ pytorch_flask.py
@@ -51,7 +51,7 @@ def hello_world():
 
 @app.route('/hello')
 def hello():
-   retrun 'Hello, world!'
+   retun 'Hello, world!'
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0',port=8005, debug=True)
